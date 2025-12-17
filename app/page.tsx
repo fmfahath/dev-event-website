9616497eda4a2c818d9dbbecc4dee1c1891f0a45
fmfahath <fmfahath.dev@gmail.com>
2025-12-17 21:48:24 +0530

    feat(hearo-section): featured event section added in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,19 @@ const Page = () => {
             <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss </h1>
             <p className={'text-center mt-5'}>Hackathons, Meetups and Conference, All in One Place</p>
             <ExploreBtn/>
+
+            {/*feature events section*/}
+            <div className={"mt-20 space-y-7"}>
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {[1,2,3,4,5].map(event=>(
+                        <li key={event}>Event {event}</li>
+                    ))}
+                </ul>
+            </div>
         </section>
-    )
+
+    );
 }
 export default Page
