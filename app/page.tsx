4a9b383bf34080314a9dd53aba0deef9c7e3fdce
fmfahath <fmfahath.dev@gmail.com>
2025-12-17 21:42:49 +0530

    feat(hearo-section): explore button created in components, adds in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ExploreBtn from "@/components/ExploreBtn";
 
 const Page = () => {
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss </h1>
             <p className={'text-center mt-5'}>Hackathons, Meetups and Conference, All in One Place</p>
+            <ExploreBtn/>
         </section>
     )
 }
