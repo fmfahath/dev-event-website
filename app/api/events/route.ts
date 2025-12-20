a3e3a84d973a8ad5244f82b0caa1c422d6d0f176
fmfahath <fmfahath.dev@gmail.com>
2025-12-20 23:43:14 +0530

    feat(api-endpoints): POST api handler created for events

---
+++ app/api/events/route.ts
@@ -0,0 +1,27 @@
+import {NextRequest, NextResponse} from "next/server";
+import connectDB from "@/lib/mongodb";
+import Event from "@/databse/event.model"
+
+export async function POST(req:NextRequest){
+    try{
+        await connectDB();
+
+        const formData = await req.formData();
+
+        let event;
+
+        try{
+            event = Object.fromEntries(formData.entries());
+        }
+        catch(e){
+            return NextResponse.json({message: 'Invalid JSON format'}, {status: 400});
+        }
+
+        const createdEvent = await Event.create(event);
+
+        return NextResponse.json({message: 'Event created successfully', event: createdEvent}, {status:201});
+    }
+    catch(e){
+        return NextResponse.json({message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'unknown' }, {status: 500});
+    }
+}
