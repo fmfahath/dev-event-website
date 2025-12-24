54bfd9d7b7800aaeb5e3e8cf866a1785303ffc19
fmfahath <fmfahath.dev@gmail.com>
2025-12-25 01:47:45 +0530

    fix(event-details-page): list item bullet point issue fixed
    - add a list-none classname in route page ul element

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Page = async() => {
             <div className={"mt-20 space-y-7"}>
                 <h3>Featured Events</h3>
 
-                <ul className="events">
+                <ul className="events list-none">
                     {events && events.length > 0 &&  events.map((event: IEvent)  =>(
                         <li key={event.title}>
                             <EventCard {...event} />
